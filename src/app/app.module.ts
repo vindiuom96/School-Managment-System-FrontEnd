603759702921bcc4a50f24f45cd2e014fc55a39f
Vindya Perera <vindipererauom@gmail.com>
2019-01-19 19:39:07 +0530

    අ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { SnotifyModule, SnotifyService, ToastDefaults } from 'ng-snotify';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SnotifyModule, SnotifyService, ToastDefaults } from 'ng-snotify';
 import { ChartsModule } from 'ng2-charts/ng2-charts';
 
-import { ApiService } from './services/api.service';
-import { AfterloginService } from './services/afterlogin.service';
-import { BeforeloginService } from './services/beforelogin.service';
-
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { NavComponent } from './components/nav/nav.component';
+import { AppComponent } from './app.component';
+import { AttendanceComponent } from './components/attendance/attendance.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component';
-import { RequestResetComponent } from './components/password/request-reset/request-reset.component';
-import { ResponseResetComponent } from './components/password/response-reset/response-reset.component';
-import { UsersComponent } from './components/users/users.component';
-import { SignupConfrimComponent } from './components/signup-confrim/signup-confrim.component';
-import { RolesComponent } from './components/roles/roles.component';
-import { PermissionsComponent } from './components/permissions/permissions.component';
-import { AttendanceComponent } from './components/attendance/attendance.component';
 import { LocationMapComponent } from './components/location-map/location-map.component';
 import { LocationComponent } from './components/location/location.component';
-import { DataService } from './services/data.service';
-import { SliderComponent } from './components/slider/slider.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavComponent } from './components/nav/nav.component';
+import { RequestResetComponent } from './components/password/request-reset/request-reset.component';
+import { ResponseResetComponent } from './components/password/response-reset/response-reset.component';
+import { PermissionsComponent } from './components/permissions/permissions.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { RolesComponent } from './components/roles/roles.component';
+import { SignupConfrimComponent } from './components/signup-confrim/signup-confrim.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { UsersComponent } from './components/users/users.component';
+import { AfterloginService } from './services/afterlogin.service';
+import { ApiService } from './services/api.service';
+import { BeforeloginService } from './services/beforelogin.service';
+import { DataService } from './services/data.service';
+
+
 
 @NgModule({
   declarations: [
--- src/app/components/attendance/attendance.component.ts
+++ src/app/components/attendance/attendance.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { CalendarEvent } from 'angular-calendar';
 
 @Component({
   selector: 'app-attendance',
