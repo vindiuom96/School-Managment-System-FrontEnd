b02cb466697cd98f347c8fa61986b8f33cdc29bd
Vindya Perera <vindipererauom@gmail.com>
2019-01-27 20:21:34 +0530

    a

--- src/app/components/subjects/subjects.component.ts
+++ src/app/components/subjects/subjects.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import { SnotifyService } from 'ng-snotify';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { TokenService } from '../../services/token.service'
 import { DataService } from 'src/app/services/data.service';
 import { RolesCheckService } from 'src/app/services/roles-check.service';
@@ -26,7 +26,7 @@ export class SubjectsComponent implements OnInit {
     'X-Requested-With' : 'XMLHttpRequest'
   }
 
-  constructor(private role : RolesCheckService,private data: DataService , private token : TokenService, private router : Router,private api : ApiService, private notify : SnotifyService) {
+  constructor(private role : RolesCheckService, private route : ActivatedRoute, private data: DataService , private token : TokenService, private router : Router,private api : ApiService, private notify : SnotifyService) {
   }
 
   ngOnInit() {
@@ -36,21 +36,42 @@ export class SubjectsComponent implements OnInit {
     this.isTeacher = this.role.isTeacher;
     this.isStudent = this.role.isStudent;
     this.isParent = this.role.isParent;
-    if(this.isParent || this.isStudent){
-      this.isStudentorParent = true;
-      this.api.get('subjects?student_id=' + localStorage.getItem('student_id'), this.headers).subscribe(
-        data => this.datahandler(data),
-        error => { this.token.remove(); this.router.navigateByUrl("/login"); }
-      );
-    } else {
-      this.notify.clear();
-    }
+
+    this.route.queryParams.subscribe(params => {
+      if(params['subject']){
+        if(this.isParent || this.isStudent){
+          this.isStudentorParent = true;
+          this.api.get('materials?student_id=' + localStorage.getItem('student_id') + '&subject_id=' + params['subject'], this.headers).subscribe(
+            data => this.datahandlerMaterial(data),
+            error => { this.token.remove(); this.router.navigateByUrl("/login"); }
+          );
+        } else {
+          this.notify.clear();
+        }
+      } else {
+        if(this.isParent || this.isStudent){
+          this.isStudentorParent = true;
+          this.api.get('subjects?student_id=' + localStorage.getItem('student_id'), this.headers).subscribe(
+            data => this.datahandler(data),
+            error => { this.token.remove(); this.router.navigateByUrl("/login"); }
+          );
+        } else {
+          this.notify.clear();
+        }
+      }
+    });
   }
 
   datahandler(data){
     this.notify.clear();
-    console.log(data);
-    this.subjects = data;
+    console.log(data[0].subject);
+    this.subjects = data[0].subject;
   }
 
+  material = null;
+  datahandlerMaterial(data){
+    this.notify.clear();
+    console.log(data);
+    this.material = data;
+  }
 }
